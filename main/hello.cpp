ca8be119321dd370bde930000dc2167ae670f52c
mohith1717 <mohithvv7@gmail.com>
2025-03-12 09:53:43 +0530

    Updated hello.cpp with second changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout<<"I AM Mohith And My SRN is PES2UG22CS641";
+  cout<<"Second changes";
   return 0;
 }
