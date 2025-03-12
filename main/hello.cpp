ebc7ba6cad34516f0fbd43ca3a837967c133f084
mohith1717 <mohithvv7@gmail.com>
2025-03-12 09:45:59 +0530

    Updated hello.cpp with recent changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout<<"I AM Mohith And My SRN is PES2UG22CS641";
   return 0;
 }
